705cdfa2711c244eebdedd6c0835d73d5ac48a72
iosアカデミア <yutamac2512@icloud.com>
2025-04-16 12:19:27 +0900

    コミット

---
+++ FirebaseApp/FirebaseApp/FirebaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAppApp.swift
+//  FirebaseApp
+//
+//  Created by Engineer MacBook Air on 2025/04/16.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
