77363a5e4e8f6b5dd28462fa76b62d4c45b90abd
iosアカデミア <yutamac2512@icloud.com>
2025-04-18 21:11:16 +0900

    コミット

--- FirebaseApp/FirebaseApp/FirebaseAppApp.swift
+++ FirebaseApp/FirebaseApp/FirebaseAppApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseAuth
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
@@ -26,11 +27,35 @@ struct YourApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                if viewModel.isAuthenticated {
-                    HelloPage(viewModel: viewModel)
-                } else {
-                    SignInView(viewModel: viewModel)
+//                if viewModel.isAuthenticated {
+//                    HelloPage(viewModel: viewModel)
+//                } else {
+//                    SignInView(viewModel: viewModel)
+//                }
+                AutomaLoggInView()
+                    .onAppear {
+                        authenticateUser()
+                    }
+            }
+        }
+    }
+    
+    func authenticateUser() {
+        if Auth.auth().currentUser == nil {
+            // 匿名認証を実行
+            Auth.auth().signInAnonymously { authResult, error in
+                if let error = error {
+                    print("匿名認証エラー: \(error.localizedDescription)")
+                    return
                 }
+                if let user = authResult?.user {
+                    print("匿名認証成功: UID - \(user.uid)")
+                }
+            }
+        } else {
+            // 既存の認証情報を利用
+            if let user = Auth.auth().currentUser {
+                print("既存のユーザー認証済み: UID - \(user.uid)")
             }
         }
     }
